2bee6fa61d3553985742efe0d4707527b0384321
CaiMeiyan <1099168914@qq.com>
2016-07-13 20:43:28 +0800

    修改了工作经历页

--- 201605cmyblog/lesson/1.debug.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by dell on 2016/7/9.
- */
-var debug=require('debug');
-
-var server=debug('server');
-server('server log');
-
--- 201605cmyblog/settings.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by dell on 2016/7/10.
- */
-module.exports={
-    secret:'cmy',
-    url:'mongodb://localhost/201605cmyblog'
-}
